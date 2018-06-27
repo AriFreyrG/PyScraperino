75a5b04f7f6f0d0de2219f04f054613bf4524b42
Ari Freyr Guðmundsson <ari.freyr.gudmundsson@advania.is>
2018-06-27 10:24:50 +0000

    added more urls

--- pyscape2.py
+++ pyscape2.py
@@ -6,7 +6,16 @@ import urllib2
 import json
 
 def main():
-    page = requests.get('https://www.mbl.is/fasteignir/leit/?q=80f323c5382397611e72800316f250d1')
+    url1 = 'https://www.mbl.is/fasteignir/leit/?q=80f323c5382397611e72800316f250d1'
+    url2 = 'https://www.mbl.is/fasteignir/leit/?page=2&q=9e2d9c3b99e4349d300c226db2e36ab8'
+    url3 = 'https://www.mbl.is/fasteignir/leit/?page=3&q=9e2d9c3b99e4349d300c226db2e36ab8'
+    SearchWars(url1)
+    SearchWars(url2)
+    SearchWars(url3)
+
+
+def SearchWars(urlboi):
+    page = requests.get(urlboi)
     tree = html.fromstring(page.content)
     openhouse = tree.xpath('//div[@class="single-realestate openhouse "]')
     houses = []
@@ -22,11 +31,12 @@ def main():
         urlstring = "https://www.mbl.is" + href[0]
 
         address = address[0].encode('utf-8')
-        properties = properties[0].encode('utf-8')
+        price = properties[0].encode('utf-8')
+        size = properties[1].encode('utf-8')
         urlstring = ''.join(urlstring)
         
         
-        megastring = ''.join(replaceSvigar(str(address))), str(fixpricestring(properties)),urlstring
+        megastring = ''.join(replaceSvigar(str(address))), str(fixpricestring(price)), str(size),urlstring 
         #jason = json.dumps(megastring)
         #print jason
         houses.append(megastring)
@@ -34,7 +44,7 @@ def main():
     printToExcel(houses)
 
 def printToExcel(houses):
-    with open('houses.csv', 'wb+') as f:
+    with open('houses.csv', 'a') as f:
             writer = csv.writer(f, delimiter=';')
             #for house in houses:
             #    writer.writerow(house)
@@ -104,6 +114,7 @@ def replacer(data):
 
 if __name__ =="__main__":
     main()
+   
 
 
 
@@ -124,7 +135,3 @@ if __name__ =="__main__":
 
 
 
-
-
-if __name__ =="__main__":
-    main()
